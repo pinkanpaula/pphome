5cacfcead48cb9b5272dcbc08e3c19b525ff69a0
Paula Pingkan <paula.pingkan@gmail.com>
2026-02-03 19:58:52 +0700

    fix routing issue

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -32,8 +32,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 497, hash: '0d06d7625cedf491fa5ea2bbfaa27eeea78c6faa745791d13d9f687926632c81', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1010, hash: '0fe40642e9e558848ecf15885f01d3224901154f5c8ae1c6d754d3f02a070f14', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 497, hash: 'da4c1927c817bbdc642881d4f6c3f6dcac6b2873af299109fcc75bc5181e8e05', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1010, hash: '419a7a3d47ebc2b9e42caf76f8faf7f4817f012a82c4029ae39da57c6995afdb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
